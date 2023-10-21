8590fed27c4dfb32a87011ad68a0a24799b0ebc0
Smorareq1 <smorareq1@gmail.com>
2023-10-21 17:30:06 -0600

    Terminado 2

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -18,17 +18,22 @@ Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
         }
 
         k = k % sz;
+        k = sz - k;
         colaAux->next = head; //Poner la cola en la cabeza
 
         
-        Node<char>* new_tail = head;
-        for (int j = 1; j < (sz - k); ++j) {
-            new_tail = new_tail->next;
-        }
+        colaAux = head; //Encontrar nueva cola
 
+        int j = 1;
+        while (j < k) {
+            colaAux = colaAux->next;
+            ++j;
+        }
         
-        head = new_tail->next;
-        new_tail->next = nullptr;
+
+       
+        head = colaAux->next;
+        colaAux->next = nullptr; //que deje de ser lista circular
 
         return head;
 }
--- PAT_HojaDeTrabajo02/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio02.cpp
@@ -2,5 +2,36 @@
 
 Node<int>* Ejercicio02::addTwoNumbers(Node<int>* l1, Node<int>* l2)
 {
-    return nullptr;
+    Node<int>* dummyHead = new Node<int>{ 0 };
+    Node<int>* current = dummyHead;
+    int carry = 0;
+
+    
+    while (l1 != nullptr || l2 != nullptr) {
+        int x = 0, y = 0;
+
+        // Si l1 no es nullptr, actualiza x y avanza a l1
+        if (l1 != nullptr) {
+            x = l1->value;
+            l1 = l1->next;
+        }
+
+        // Si l2 no es nullptr, actualiza y y avanza a l2
+        if (l2 != nullptr) {
+            y = l2->value;
+            l2 = l2->next;
+        }
+
+        int sum = x + y + carry;
+        carry = sum / 10;
+        current->next = new Node<int>{ sum % 10 };
+        current = current->next;
+    }
+
+    //Ver si necesita otro digito
+    if (carry > 0) {
+        current->next = new Node<int>{ carry };
+    }
+
+    return dummyHead->next;
 }
