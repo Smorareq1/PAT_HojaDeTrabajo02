86d9421334a887cc5a4986e5fc7331a96a3c79bf
Smorareq1 <smorareq1@gmail.com>
2023-10-20 20:01:30 -0600

    Ejercicio uno

--- PAT_HojaDeTrabajo02/Ejercicio01.cpp
+++ PAT_HojaDeTrabajo02/Ejercicio01.cpp
@@ -1,6 +1,34 @@
 #include "Ejercicio01.h"
+#include "Node.h"
 
 Node<char>* Ejercicio01::rotateRight(Node<char>* head, int k)
 {
-    return nullptr;
+    
+
+        if (head == nullptr || k == 0) {
+            return head;
+        } //caso base
+
+
+        int sz = 1;
+        Node<char>* colaAux = head;
+        while (colaAux->next != nullptr) {
+            colaAux = colaAux->next; //Ultimo nodo
+            sz++;
+        }
+
+        k = k % sz;
+        colaAux->next = head; //Poner la cola en la cabeza
+
+        
+        Node<char>* new_tail = head;
+        for (int j = 1; j < (sz - k); ++j) {
+            new_tail = new_tail->next;
+        }
+
+        
+        head = new_tail->next;
+        new_tail->next = nullptr;
+
+        return head;
 }
